5cd56a691717d9f66a62e36b62dcfc236de7a691
Raúl Caro Pastorino <tecnico@fryntiz.es>
2019-02-11 23:24:33 +0100

    Amplio estructura

---
+++ config.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+# -*- encoding: utf-8 -*-
+
+# @author     Raúl Caro Pastorino
+# @email      dev@fryntiz.es
+# @web        https://fryntiz.es
+# @gitlab     https://gitlab.com/fryntiz
+# @github     https://github.com/fryntiz
+# @twitter    https://twitter.com/fryntiz
+# @telegram   https://t.me/fryntiz
+
+# Create Date: 2019
+# Project Name: 
+# Description: 
+#
+# Dependencies: 
+#
+# Revision 0.01 - File Created
+# Additional Comments: 
+
+# @copyright  Copyright © 2019 Raúl Caro Pastorino
+# @license    https://wwww.gnu.org/licenses/gpl.txt
+
+# Copyright (C) 2019  Raúl Caro Pastorino
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+# Guía de estilos aplicada: PEP8
+
+# #           Descripción           # #
+
+# # Archivo con las configuraciones del proyecto.
+
+# #       Importar Librerías        # #
+
+
+# #             Variables           # #
+
+
+# #             Funciones           # #
+
+
+class config:
+    LCD_RS = 7
+    LCD_E = 17
+    LCD_D4 = 27
+    LCD_D5 = 22
+    LCD_D6 = 23
+    LCD_D7 = 24
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
-#git clone ????
+#!/usr/bin/env bash
+
+git clone https://gitlab.com/fryntiz/raspberry-HD44780-LCD16x2.git mylib/HD44780
+git clone https://gitlab.com/fryntiz/raspberry-MAX7219.git mylib/MAX7219
 
 ## Desplegando paquetes con pipenv
 pipenv install --deploy
